2142dbcd8980ef96b544eab82032a5a453e045cb
Dipti-Byjus <100108871+Dipti-Byjus@users.noreply.github.com>
2023-01-14 15:19:54 +0530

    Update data_preprocessing.py

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,25 +1,21 @@
 # Text Data Preprocessing Lib
-import nltk
-nltk.download('punkt')
-nltk.download('wordnet')
+
 
 # to stem words
-from nltk.stem import PorterStemmer
+
 
 # object/instance of class PorterStemmer()
-stemmer = PorterStemmer()
+
 
 # importing json lib
-import json
+
 
 # to store data into files
-import pickle
 
-import numpy as np
 
-words=[] #list of unique roots words in the data
-classes = [] #list of unique tags in the data
-pattern_word_tags_list = [] #list of the pair of (['words', 'of', 'the', 'sentence'], 'tags')
+
+
+
 
 # words to be ignored while creating Dataset
 ignore_words = ['?', '!',',','.', "'s", "'m"]
